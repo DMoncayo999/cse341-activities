d679ab8b5dcdf6eb9a37c099fc29c90ec080fb20
DELFINA MONCAYO <mon19012@byui.edu>
2021-04-22 21:51:54 -0400

    new routes file pr01 and views for input and show data assigm1

---
+++ routes/pr01.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req, res, next) => {
+    res.render('forms/twoinputs', { title: 'Assignment 01',  path: '/pr01'  } );
+});
+
+router.post('/',(req, res, next) => {
+    res.render('forms/showData', { title: 'Assignment 01',  path: '/pr01', fname: req.body.fn, lname: req.body.ln } );
+});
+
+module.exports = router;
